6422232ea0d91e0c7fbf65b908615cd0995244be
Ben <koppe.development@gmail.com>
2025-11-09 21:56:28 -0500

    Strip default devenv

--- devenv.nix
+++ devenv.nix
@@ -1,8 +1,11 @@
-{ pkgs, lib, config, inputs, ... }:
+{
+  pkgs,
+  ...
+}:
 
 {
   # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  # env.GREET = "devenv";
 
   # https://devenv.sh/packages/
   packages = [ pkgs.git ];
@@ -17,15 +20,10 @@
   # services.postgres.enable = true;
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
+  scripts.hello.exec = '''';
 
   # https://devenv.sh/basics/
-  enterShell = ''
-    hello         # Run scripts directly
-    git --version # Use packages
-  '';
+  enterShell = '''';
 
   # https://devenv.sh/tasks/
   # tasks = {
