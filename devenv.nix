f77d2541140427b41abbc358c9c1557732914f5e
Ben <koppe.development@gmail.com>
2025-11-09 22:25:42 -0500

    Set up devenv

--- devenv.nix
+++ devenv.nix
@@ -2,13 +2,32 @@
   pkgs,
   ...
 }:
-
+let
+  browsers =
+    (builtins.fromJSON (builtins.readFile "${pkgs.playwright-driver}/browsers.json")).browsers;
+  chromium-rev = (builtins.head (builtins.filter (x: x.name == "chromium") browsers)).revision;
+in
 {
   # https://devenv.sh/basics/
   # env.GREET = "devenv";
+  env = {
+    PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright.browsers}";
+    PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = true;
+    PLAYWRIGHT_NODEJS_PATH = "${pkgs.nodejs}/bin/node";
+    PLAYWRIGHT_LAUNCH_OPTIONS_EXECUTABLE_PATH = "${pkgs.playwright.browsers}/chromium-1187/chrome-linux/chrome";
+  };
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = with pkgs; [
+    just
+  ];
+
+  dotenv.disableHint = true;
+
+  languages.python = {
+    enable = true;
+    uv.enable = true;
+  };
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
